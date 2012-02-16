9dee5a9746628ae5eacbd483df89ec90b1819d20
Amit Kumar <stud.amitkumar@gmail.com>
2012-02-16 21:10:34 +0530

    Signed-off-by: Amit Kumar <stud.amitkumar@gmail.com>

--- bin/MyApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\bin\MyApp-debug-unaligned.apk : \
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\bin\MyApp.ap_ \
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\bin\classes.dex \
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\libs\phonegap-0.9.3.jar \
--- bin/classes.dex.d
+++
@@ -1,2 +0,0 @@
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\bin\classes.dex : \
-C:\Documents and Settings\LetsGoMo 1\Desktop\Try_script\DirectoryListingPhoneGapPlugin\libs\phonegap-0.9.3.jar \
